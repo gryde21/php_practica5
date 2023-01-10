1f7d9d2fa99090e990a6f3f0ecc2ef6a6f28885f
gryde21 <122233193+gryde21@users.noreply.github.com>
2023-01-10 22:34:26 +0100

    Add files via upload

---
+++ dades.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href="style/index.css">
+    <title>Datos</title>
+</head>
+<body>
+    <?php
+        echo '
+            <p>Nom:</p>
+                <!--peremenaia s imeniem s <b> v chernom-->
+            <p>Cognoms:</p>
+                <!--peremenaia s familiei s <b> v chernom-->
+            <a href="index.php">Inici</a>';
+    ?>
+</body>
+</html>
